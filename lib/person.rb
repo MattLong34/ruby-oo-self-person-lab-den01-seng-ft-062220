882f5f35462cb8eae2c93ef3bb58c753c7876ec5
Matt Long <mdlong34@gmail.com>
2020-06-29 15:49:59 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,35 @@
-# your code goes here
+require 'pry'
+
+class Person
+  
+  attr_accessor :dollars, :happiness_points, :hygiene_points
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize name
+    @name = name
+    @dollars = 25
+    @happiness_points = 8
+    @hygiene_points = 8
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def happy?
+    happiness_points > 7
+  end 
+  
+  # def happy?
+  #   happiness > 7
+  # end
+  
+  binding.pry
+  
+  
+end
+
+  Matt = Person.new "Matt"
